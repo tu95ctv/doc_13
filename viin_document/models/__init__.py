42a964c83c36b777ed52011e4f1c20c35a426635
Nguyễn Đức Tứ <tund@nhanlucsieuviet.com>
2021-07-21 16:07:51 +0700

    [RMV] delete_documents_mixin

--- viin_document/models/__init__.py
+++ viin_document/models/__init__.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import mail_activity
-from . import ir_attachment
 from . import document
-from . import documents_mixin
 from . import folder
 from . import res_partner
 from . import share
